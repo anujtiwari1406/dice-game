7ab09cab56a63de36f61df647e52c4a8d8044a58
Anuj Tiwari <168212574+anujtiwari1406@users.noreply.github.com>
2024-07-13 18:53:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+//Image 1
+var randomnumber1 = Math.floor(Math.random()*6)+1;
+var randomdiceimage1 = "dice" + randomnumber1 + ".png"; 
+var imageSource1 = "images/" + randomdiceimage1; 
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", imageSource1);
+
+
+//Image 2
+var randomnumber2 = Math.floor(Math.random()*6)+1;
+var randomdiceimage2 = "dice" + randomnumber2 + ".png"; 
+var imageSource2 = "images/" + randomdiceimage2; 
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", imageSource2)  
+
+//winner announcement
+if (randomnumber1 > randomnumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 Win ";
+
+}
+else if (randomnumber2 > randomnumber1){
+    document.querySelector("h1").innerHTML = "🚩Player 2 Win "
+
+}
+
+else {
+    document.querySelector("h1").innerHTML = "Draw"
+
+}
+
+
