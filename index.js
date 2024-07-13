c0a1bc2cabaa6b22f491b59c258568f25bfd118f
Anuj Tiwari <168212574+anujtiwari1406@users.noreply.github.com>
2024-07-13 19:46:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var randomnumber1 = Math.floor(Math.random()*6)+1;
 var randomdiceimage1 = "dice" + randomnumber1 + ".png"; 
 
 var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src",randomdiceimage1);
+image1.setAttribute("src", randomdiceimage1);
 
 
 //Image 2
@@ -12,7 +12,7 @@ var randomnumber2 = Math.floor(Math.random()*6)+1;
 var randomdiceimage2 = "dice" + randomnumber2 + ".png"; 
 
 var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src",randomdiceimage2)  
+image2.setAttribute("src", randomdiceimage2)  
 
 //winner announcement
 if (randomnumber1 > randomnumber2){
